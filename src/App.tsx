8aaf8243c82425a81ddf3f2fb70359cfe8345de1
Yomna Ali <yomnaaly83@gmail.com>
2024-08-16 00:16:57 +0300

    feat(app router) : create router by createBrowserRouter and provide the app router to RouterProvider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,52 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Body from "./components/Body";
+import LandingPage from "./components/LandingPage";
+import MainPage from "./components/MainPage";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import RestaurantMenu from "./components/RestaurantMenu";
+import SignIn from "./components/SignIn";
+import Cart from "./components/Cart";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/auth",
+        element: <LandingPage />,
+      },
+      {
+        path: "/city/:cityName",
+        element: <MainPage />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurants/:rsId",
+        element: <RestaurantMenu />,
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+    errorElement: <div>Error</div>,
+  },
+]);
 function App() {
-  return (
-    <div>
-      <h1 className="head">Header</h1>
-    </div>
-  );
+  return <RouterProvider router={appRouter} />;
 }
 
 export default App;
