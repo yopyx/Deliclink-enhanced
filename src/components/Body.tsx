18a1c349fa56ef7c90b63af79c5efa0c82cccabc
Yomna Ali <yomnaaly83@gmail.com>
2024-08-16 21:46:57 +0300

    feat(Body ui structure) : Body component structure consists of Header component and Outlet component as a filling structure and Footer component

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,5 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+
 const Body = () => {
-  return <div>Body</div>;
+  return (
+    <div className="flex flex-col">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default Body;
