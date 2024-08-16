d12925c0402edc377c70d6f6fdab75fc18b3bb29
Yomna Ali <yomnaaly83@gmail.com>
2024-08-16 21:33:45 +0300

    feat(constants) : create constants file and add some apis (menu api and geo api) and some urls to defining options icons

---
+++ src/utils/constants.ts
@@ -0,0 +1,28 @@
+export const CORS = "https://corsproxy.io/?";
+export const RES_LINK = [
+  "https://www.swiggy.com/dapi/restaurants/list/v5?",
+  "&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+];
+
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const MENU_API = [
+  `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.9006311&lng=75.923534&restaurantId=`,
+  `&submitAction=ENTER`,
+];
+
+export const GEO_API = [
+  "https://api.opencagedata.com/geocode/v1/json?q=",
+  `&key=${import.meta.env.REACT_APP_GEO_KEY}`,
+];
+export const PROFILE_ICON =
+  "https://static-00.iconduck.com/assets.00/profile-default-icon-144x144-030q38zb.png";
+export const RIGHT_ARROW_ICON =
+  "https://static-00.iconduck.com/assets.00/arrow-right-1-icon-72x144-fo0m2d6f.png";
+export const LEFT_ARROW_ICON =
+  "https://static-00.iconduck.com/assets.00/arrow-left-1-icon-71x144-e2eaygf9.png";
+export const FILTER_ICON =
+  "https://static-00.iconduck.com/assets.00/filter-icon-144x140-jvrc9le0.png";
+export const DOWN_ARROW_ICON =
+  "https://static-00.iconduck.com/assets.00/arrow-down-1-icon-144x73-ywwkdgwd.png";
