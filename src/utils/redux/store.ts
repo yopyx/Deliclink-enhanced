e6774ae8580070757c57a79b685fa0f4aef0cf39
Yomna Ali <yomnaaly83@gmail.com>
2024-08-20 23:18:51 +0300

    feat(typing useDispatch and useSelector hooks) : export RootState and AppDispatch types from redux store and use typed hooks useAppDispatch and useAppSelector instead of the default hooks

--- src/utils/redux/store.ts
+++ src/utils/redux/store.ts
@@ -3,3 +3,5 @@ import { configureStore } from "@reduxjs/toolkit";
 export const appStore = configureStore({
   reducer: {},
 });
+export type RootState = ReturnType<typeof appStore.getState>;
+export type AppDispatch = typeof appStore.dispatch;
