261d8ea30a476c3060a41e4aa76589bf0665060b
Yomna Ali <yomnaaly83@gmail.com>
2024-08-20 06:15:47 +0300

    feat(query client) : provide the app queryClient which is a central instance for managing state and configuration of queries and mutations in react query

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./final.css";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>
 );
