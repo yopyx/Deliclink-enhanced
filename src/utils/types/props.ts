9e6b465d586c40bfcbe18ed7ab6f1b665b84067e
Yomna Ali <yomnaaly83@gmail.com>
2024-08-23 23:53:24 +0300

    feat(typing props) : create CuisinesSectionProps type in props file for typing props of CuisinesSuggestions component

---
+++ src/utils/types/props.ts
@@ -0,0 +1,57 @@
+import { Cuisine } from "./fetchedData";
+
+export type CuisinesSectionProps = {
+  info: {
+    "@type": string;
+    header: {
+      title: string;
+      headerStyling: {
+        padding: {
+          left: number;
+          top: number;
+          bottom: number;
+        };
+      };
+    };
+    layout: {
+      rows: number;
+      columns: number;
+      horizontalScrollEnabled: boolean;
+      itemSpacing: number;
+      widgetPadding: object;
+      containerStyle: {
+        containerPadding: {
+          left: number;
+          top: number;
+          right: number;
+          bottom: number;
+        };
+      };
+      scrollBar: object;
+      widgetTheme: {
+        defaultMode: {
+          backgroundColour: string;
+          theme: string;
+        };
+        darkMode: {
+          theme: string;
+        };
+      };
+    };
+    imageGridCards: {
+      info: Cuisine[];
+      style: {
+        width: {
+          type: string;
+          value: number;
+          reference: string;
+        };
+        height: {
+          type: string;
+          value: number;
+          reference: string;
+        };
+      };
+    };
+  };
+};
