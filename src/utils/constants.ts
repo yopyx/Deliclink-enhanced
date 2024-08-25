4a03d43c6706b654c2b19467186a1b033a838a8e
Yomna Ali <yomnaaly83@gmail.com>
2024-08-25 06:34:47 +0300

    feat(constants) : customize RES_CARDS_UPDATE_API variable into a function with lat/lng parameters that returns url for updating list of restaurants data

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,9 +2,11 @@ export const CORS = "https://corsproxy.io/?";
 export const CITY_RES_API = (lat: string, lng: string) =>
   `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
 
-export const RES_CARDS_UPDATE_API =
-  "https://www.swiggy.com/dapi/restaurants/list/update";
+// export const RES_CARDS_UPDATE_API =
+//   "https://www.swiggy.com/dapi/restaurants/list/update";
 
+export const RES_CARDS_UPDATE_API = (lat: string, lng: string) =>
+  `https://www.swiggy.com/api/seo/getListing?lat=${lat}&lng=${lng}&isDineoutCollection=false`;
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
@@ -19,6 +21,7 @@ export const GEO_API = (address: string) =>
     .replace(/ /g, "+")}&key=${
     import.meta.env.VITE_APP_GEO_KEY
   }&language=en&no_annotations=1&pretty=1`;
+
 export const PROFILE_ICON =
   "https://static-00.iconduck.com/assets.00/profile-default-icon-144x144-030q38zb.png";
 
