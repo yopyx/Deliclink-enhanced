104b7a4a1b3a7170d5b2550dd1ed8a7e53b72b5a
Yomna Ali <yomnaaly83@gmail.com>
2024-08-27 06:46:49 +0300

    feat(FilterOption data layer) : receive info prop and render the data along UI structure and receive handleView prop to handle closure of the filter options partition

--- src/components/FilterOption.tsx
+++ src/components/FilterOption.tsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import { FilterOptionProps } from "../utils/types/props";
 
-const FilterOption = () => {
+const FilterOption = ({ info, handleView }: FilterOptionProps) => {
   const [viewedOption, setViewedOption] = useState("Sort");
   return (
-    <div className="fixed top-0 z-20 w-screen h-screen bg-black bg-opacity-30 -ml-[16.5%]">
-      <div className="bg-white sticky top-36 rounded-xl mx-auto my-auto w-2/5 p-4">
+    <div className="fixed top-0 w-screen h-screen z-20 -ml-[16.5%]">
+      <div className="bg-white sticky top-36 rounded-xl mx-auto my-auto w-2/5 p-4 z-30">
         <div className="flex justify-between font-bold text-xl text-stone-700 border-b-2 pb-1">
           <h2 className="">Filter</h2>
-          <button className="bg-stone-200 rounded-full px-2">x</button>
+          <button
+            className="bg-stone-200 rounded-full px-2"
+            onClick={() => handleView(false)}
+          >
+            x
+          </button>
         </div>
         <div className="flex m-2">
           <div className="flex flex-col space-y-4 font-semibold text-stone-500 border-r-2 pr-5">
@@ -22,7 +28,7 @@ const FilterOption = () => {
               Sort
             </button>
             <>
-              {[]?.map((e, i) => (
+              {info?.facetList?.map((e) => (
                 <button
                   className={`text-left${
                     viewedOption === e?.label ? " text-orange-500" : ""
@@ -37,9 +43,9 @@ const FilterOption = () => {
               ))}
             </>
           </div>
-          <div className="flex flex-col pl-10 h-80 w-[77%] overflow-y-scroll font-light space-y-2 text-stone-400">
+          <div className="flex flex-col pl-10 h-80 w-[77%] overflow-auto font-light space-y-2 text-stone-400">
             {viewedOption === "Sort"
-              ? [].map((e) => (
+              ? info.sortConfigs.map((e) => (
                   <div className="flex space-x-2" key={e?.key}>
                     <input
                       type="radio"
@@ -49,19 +55,24 @@ const FilterOption = () => {
                     <label>{e?.title}</label>
                   </div>
                 ))
-              : [].map((e) => (
-                  <div className="flex space-x-2" key={e?.id}>
-                    <input
-                      type="checkbox"
-                      className="text-left accent-orange-500"
-                    />
-                    <label>{e?.label}</label>
-                  </div>
-                ))}
+              : info?.facetList
+                  .find((e) => e.label === viewedOption)
+                  ?.facetInfo.map((e, i) => (
+                    <div className="flex space-x-2" key={e?.id + i}>
+                      <input
+                        type="checkbox"
+                        className="text-left accent-orange-500"
+                      />
+                      <label>{e?.label}</label>
+                    </div>
+                  ))}
           </div>
-          <button className="bg-slate-600">Apply</button>
         </div>
       </div>
+      <div
+        className="absolute top-0 w-screen h-screen bg-black bg-opacity-30"
+        onClick={() => handleView(false)}
+      ></div>
     </div>
   );
 };
