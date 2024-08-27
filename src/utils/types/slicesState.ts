a476d753fe7f257049b8ef100b306550cb2ac9c8
Yomna Ali <yomnaaly83@gmail.com>
2024-08-27 21:44:16 +0300

    feat(typing state) : create FilterState type that has sortKey attribute of string type and facet attribute of Facet type. FilterState type is used to type -to be created- filter slice state

--- src/utils/types/slicesState.ts
+++ src/utils/types/slicesState.ts
@@ -1,7 +1,15 @@
-export type GeoLocationState = {
+export type GeoLocationStateProp = {
   city: string;
   geometry: {
     lat: string;
     lng: string;
   };
+};
+
+export type Facet = {
+  [id: string]: { value: string }[];
+};
+export type FilterState = {
+  sortKey: string;
+  facet: Facet;
 };
