1141b0b87122c9f9793cb30ba9ad6a9bb1e82e09
Yomna Ali <yomnaaly83@gmail.com>
2024-08-27 21:52:25 +0300

    feat(link filter slice with app store) : add filter slice reducer to app store reducer

--- src/utils/redux/store.ts
+++ src/utils/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import geoLocationReducer from "./geoLocationsSlice";
+import filterReducer from "./filterSlice";
 
 export const appStore = configureStore({
   reducer: {
     geoLocation: geoLocationReducer,
+    filter: filterReducer,
   },
 });
 export type RootState = ReturnType<typeof appStore.getState>;
