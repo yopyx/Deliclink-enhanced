a91595bdaaed9eef34a62429dd29d49abb40a5a5
Yomna Ali <yomnaaly83@gmail.com>
2024-08-29 05:51:10 +0300

    feat(refactor data type rendered) : use isCityResData function to detect the type of data being rendered in MainPage component

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -8,15 +8,17 @@ import { useAppSelector } from "../utils/types/reactReduxHooks";
 import { GeoLocationStateProp } from "../utils/types/slicesState";
 import FilterBar from "./FilterBar";
 import { Link } from "react-router-dom";
+import { isCityResData } from "../utils/constants";
 
 const MainPage = () => {
-  const { city, geometry } = useAppSelector(
+  const { geometry } = useAppSelector(
     (store) => store.geoLocation.currentLocation
   ) as GeoLocationStateProp;
   const { data, status, error } = useQuery({
     queryKey: ["city data", geometry.lat],
     queryFn: () => getCityResData(geometry.lat, geometry.lng),
   });
+
   if (status === "pending") {
     return <div>loading...</div>;
   }
@@ -25,11 +27,15 @@ const MainPage = () => {
   }
   return (
     <div className="w-[85%] my-10 mx-auto mr-0 flex flex-col space-y-10 overflow-x-hidden">
-      <CuisinesSuggestions info={data!.data.cards[0].card.card} />
+      {isCityResData(data) && (
+        <CuisinesSuggestions info={data.data.cards[0].card.card} />
+      )}
       <div className="flex flex-col w-[85%] overflow-x-hidden border-b-2 border-stone-300 pb-10">
         <div className="flex justify-between">
           <h2 className="font-semibold text-xl">
-            {data?.data.cards[1].card.card.header.title}
+            {isCityResData(data)
+              ? data.data.cards[1].card.card.header.title
+              : data!.data.cards[0].card.card.title}
           </h2>
           <div className="flex space-x-3">
             <button className="font-bold text-xl text-stone-500 rounded-full p-2 bg-stone-300 bg-opacity-70 disabled:opacity-50">
@@ -49,24 +55,37 @@ const MainPage = () => {
           </div>
         </div>
         <div className={`flex w-max relative overflow-x-hidden duration-300`}>
-          {data!.data.cards[1].card.card.gridElements.infoWithStyle.restaurants.map(
-            (e, i) => (
-              <Link key={e?.info?.id + i} to={"/restaurants/" + e?.info?.id}>
-                <RestaurantCard resData={e}></RestaurantCard>
-              </Link>
-            )
-          )}
+          {(isCityResData(data)
+            ? data!.data.cards[1].card.card.gridElements.infoWithStyle
+                .restaurants
+            : data!.data.cards[2].card.card.gridElements.infoWithStyle
+                .restaurants
+          ).map((e, i) => (
+            <Link key={e?.info?.id + i} to={"/restaurants/" + e?.info?.id}>
+              <RestaurantCard resData={e}></RestaurantCard>
+            </Link>
+          ))}
         </div>
       </div>
       <div className="space-y-5">
         <h2 className="font-semibold text-xl">
-          {data?.data.cards[2].card.card.title}
+          {isCityResData(data) && data.data.cards[2].card.card.title}
         </h2>
-        <FilterBar info={data!.data.cards[3].card.card} />
+        <FilterBar
+          info={
+            isCityResData(data)
+              ? data!.data.cards[3].card.card
+              : data!.data.cards[1].card.card
+          }
+        />
       </div>
       <RestaurantCardsContainer
         dataList={
-          data!.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+          isCityResData(data)
+            ? data!.data.cards[4].card.card.gridElements.infoWithStyle
+                .restaurants
+            : data!.data.cards[2].card.card.gridElements.infoWithStyle
+                .restaurants
         }
       />
     </div>
