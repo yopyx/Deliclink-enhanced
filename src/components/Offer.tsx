7dbce7882d5dd138b61f378bc137ae42e87bec0a
Yomna Ali <yomnaaly83@gmail.com>
2024-08-29 23:44:05 +0300

    feat(Offer UI layer) : create UI layer structure of Offer Component that represents a section in RestaurantMenu component

---
+++ src/components/Offer.tsx
@@ -0,0 +1,11 @@
+const Offer = () => {
+  return (
+    <div className="bg-[#eeeeee80] w-[185px] h-[100px] p-2 mx-2 text-center border-4 border-dashed border-stone-400">
+      <h4>header</h4>
+      <h6>couponCode</h6>
+      <h6>description</h6>
+    </div>
+  );
+};
+
+export default Offer;
