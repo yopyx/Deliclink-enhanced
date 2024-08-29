590035ba9dd14d85a85652b0e949cbf5c58ffbb2
Yomna Ali <yomnaaly83@gmail.com>
2024-08-30 01:50:09 +0300

    feat(query RestaurantMenu data) : query the data via useQuery hook which would call getMenuData function with all valid parameters, location geometry (lat/lng) are supplied by useAppSelector hook and resId is supplied by useParams hook

--- src/components/RestaurantMenu.tsx
+++ src/components/RestaurantMenu.tsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 import Offer from "./Offer";
 import MenuCategory from "./MenuCategory";
+import { useQuery } from "@tanstack/react-query";
+import getMenuData from "../utils/functions/getMenuData";
+import { useAppSelector } from "../utils/types/reactReduxHooks";
+import { GeoLocationStateProp } from "../utils/types/slicesState";
 
 const RestaurantMenu = () => {
   const [veg, setVeg] = useState(false);
+  const { geometry } = useAppSelector(
+    (store) => store.geoLocation.currentLocation
+  ) as GeoLocationStateProp;
+  const { resId } = useParams();
+  const { data, status, error } = useQuery({
+    queryKey: ["menu", resId],
+    queryFn: () => getMenuData(geometry.lat, geometry.lng, resId || ""),
+    enabled: resId !== undefined,
+  });
   return (
     <div className="menu-page block w-w1000 mx-auto">
       <div className="res-info mt-14 p-3 bg-[#ffddcd] border-2 border-st_orange">
