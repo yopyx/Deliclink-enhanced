cc11a8bee6338eb95d581622527d9ec6bf880c1c
Yomna Ali <yomnaaly83@gmail.com>
2024-08-29 05:47:40 +0300

    feat(getCityResData response data type) : type response data as union of CityResData and CityResDatav2 types

--- src/utils/functions/getCityResData.ts
+++ src/utils/functions/getCityResData.ts
@@ -1,4 +1,4 @@
-import { CityResData } from "../types/fetchedData";
+import { CityResData, CityResDatav2 } from "../types/fetchedData";
 import { CITY_RES_API, CORS } from "../constants";
 
 const getCityResData = async (lat: string, lng: string) => {
@@ -6,7 +6,8 @@ const getCityResData = async (lat: string, lng: string) => {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    return data as CityResData;
+    console.log(data);
+    return data as CityResData | CityResDatav2;
   } catch (error) {
     console.error("Error fetching restaurants:", error);
   }
