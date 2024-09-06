d18dc0fcad36b1f0c25a22c2c1da29c358cdfc43
Yomna Ali <yomnaaly83@gmail.com>
2024-09-06 22:04:56 +0300

    feat(Offer component data layer) : receive info props and destructure the data to be rendered along UI layer

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,9 +1,12 @@
-const Offer = () => {
+import { OfferProps } from "../utils/types/props";
+
+const Offer = ({ info }: OfferProps) => {
+  const { header, couponCode, description } = info;
   return (
     <div className="bg-[#eeeeee80] w-[185px] h-[100px] p-2 mx-2 text-center border-4 border-dashed border-stone-400">
-      <h4>header</h4>
-      <h6>couponCode</h6>
-      <h6>description</h6>
+      <h4>{header}</h4>
+      <h6>{couponCode}</h6>
+      <h6>{description ? "| " + description + " |" : ""}</h6>
     </div>
   );
 };
