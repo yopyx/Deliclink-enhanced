a9729b9866d375a8699a8915631bc8ab07044bac
Yomna Ali <yomnaaly83@gmail.com>
2024-09-06 22:23:18 +0300

    feat(MenuCategory data layer) : receive props, index and setViewCategory props are passed to make sure one category list is opened at a time while isShown showing the initial state of viewCategory and isVeg prop value toggles between rendering only-veg list or whole list and category prop which represents the main data that is passed into FoodItem component as info prop

--- src/components/MenuCategory.tsx
+++ src/components/MenuCategory.tsx
@@ -1,22 +1,37 @@
+import { MenuCategoryProps } from "../utils/types/props";
 import FoodItem from "./FoodItem";
 
-const MenuCategory = () => {
+const MenuCategory = ({
+  index,
+  category,
+  isVeg,
+  isShown,
+  setViewCategory,
+}: MenuCategoryProps) => {
+  const vegList = category.filter((item) => isVeg && item.card.info.isVeg);
   return (
     <div className="menu-category inl bg-slate-50/50 rounded-lg mb-6">
-      ({" "}
-      <h3
-        className="category-title w-full inline-block font-bold text-lg cursor-pointer bg-[#efcbbc] px-1 border-2 rounded-lg hover:bg-st_orange hover:text-white"
-        data-testid="category"
-      >
-        <span>veg list number</span>
-        <span className="float-right mr-2 text-gray-500 font-thin text-2xl">
-          ⋁
-        </span>
-      </h3>
-      )
-      <div className="mb-4">
-        {[].map((item) => (
-          <FoodItem />
+      {!isVeg || (isVeg && vegList.length) ? (
+        <h3
+          className="category-title w-full inline-block font-bold text-lg cursor-pointer bg-[#efcbbc] px-1 border-2 rounded-lg hover:bg-st_orange hover:text-white"
+          data-testid="category"
+          onClick={() => setViewCategory(isShown ? "" : index)}
+        >
+          <span>{`${category[0].card.info.category} (${
+            isVeg ? vegList.length : category.length
+          })`}</span>
+          <span className="float-right mr-2 text-gray-500 font-thin text-2xl">
+            {isShown ? "⋁" : "⋀"}
+          </span>
+        </h3>
+      ) : (
+        ""
+      )}
+      <div className={(isShown ? "" : "hidden ") + "mb-4"}>
+        {(isVeg ? vegList : category).map((item) => (
+          <div key={item.card.info.id}>
+            <FoodItem info={item.card.info} />
+          </div>
         ))}
       </div>
     </div>
