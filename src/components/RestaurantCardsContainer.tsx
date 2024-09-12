42b1e662b0aa9b031f1221a62bd9743cc0cd920e
Yomna Ali <yomnaaly83@gmail.com>
2024-09-12 21:19:21 +0300

    feat(RestaurantCardsContainer data layer) : query paginated restaurant cards data via useInfiniteQuery hook which calls getResCardsData async function with location geometry (lat/lng) and next page token and render the memoized fetched data along dataList prop

--- src/components/RestaurantCardsContainer.tsx
+++ src/components/RestaurantCardsContainer.tsx
@@ -1,14 +1,83 @@
 import RestaurantCard from "./RestaurantCard";
 import { Link } from "react-router-dom";
+import { RestaurantCardsContainerProps } from "../utils/types/props";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import getResCardsData from "../utils/functions/getResCardsData";
+import { useMemo } from "react";
+import { isGridCard2 } from "../utils/constants";
 
-const RestaurantCardsContainer = () => {
+const RestaurantCardsContainer = ({
+  dataList,
+  lat,
+  lng,
+  dataObj,
+  sortConfig,
+  facet,
+}: RestaurantCardsContainerProps) => {
+  const {
+    data,
+    error,
+    status,
+    isFetching,
+    isLoading,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: [
+      "restaurants list",
+      lat,
+      JSON.stringify(sortConfig),
+      JSON.stringify(facet),
+    ],
+    queryFn: ({ pageParam }) => getResCardsData(lat, lng, pageParam),
+    initialPageParam: dataObj,
+    getNextPageParam: (prevData) => {
+      return (
+        (prevData?.data.success.pageOffset.nextOffset && {
+          sortAttribute: sortConfig.sortKey,
+          facet,
+          isFiltered:
+            JSON.stringify({ sortConfig, facet }) !==
+            '{"sortConfig":{"sortTitle":"Relevance(Default)","sortKey":"relevance"},"facet":{}}',
+          queryId: dataObj.queryId,
+          seoParams: dataObj.seoParams,
+          nextOffset: prevData.data.success.pageOffset.nextOffset,
+          widgetOffset: prevData.data.success.pageOffset.widgetOffset,
+        }) ||
+        undefined
+      );
+    },
+  });
+  const updatedList = useMemo(() => {
+    return (
+      data?.pages.flatMap(
+        (page) =>
+          page?.data.success.cards.find((e) => isGridCard2(e))?.card?.card
+            ?.gridElements?.infoWithStyle?.restaurants
+      ) || []
+    );
+  }, [data]);
+  if (status === "pending") {
+    return <div>loading...</div>;
+  }
+  if (status === "error") {
+    return <div>{JSON.stringify(error)}</div>;
+  }
   return (
     <div className="flex flex-wrap">
-      {[].map((e, i) => (
-        <Link key={e?.info?.id + i} to={"/restaurants/" + e?.info?.id}>
-          <RestaurantCard resData={e}></RestaurantCard>
+      {[
+        ...((JSON.stringify({ sortConfig, facet }) ===
+          '{"sortConfig":{"sortTitle":"Relevance(Default)","sortKey":"relevance"},"facet":{}}' &&
+          dataList) ||
+          []),
+        ...updatedList,
+      ].map((c, i) => (
+        <Link key={c!.info.id + i} to={"/restaurants/" + c!.info.id}>
+          <RestaurantCard resData={c!}></RestaurantCard>
         </Link>
       ))}
+      {isFetchingNextPage && <div>Loading...</div>}
     </div>
   );
 };
