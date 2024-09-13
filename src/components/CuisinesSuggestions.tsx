2c347aa4651333f59c1e3f998ca8e22236adbcac
Yomna Ali <yomnaaly83@gmail.com>
2024-09-13 23:31:06 +0300

    feat(horizontal scrolling in CuisinesSuggestions) : create translateValue and maxTranslation states via useState hook in addition to establishing scroll ref via useRef hook and create leftScroll and rightScroll functions to control the left and right scrolling of the scroll ref and allow calling them at onClick event of button elements and create handleScroll function to set states to the current scroll value and allow calling this function at onScroll event of the div element containing the cards list in addition to adding to the div ref attribute (cuisinesScrollRef)

--- src/components/CuisinesSuggestions.tsx
+++ src/components/CuisinesSuggestions.tsx
@@ -1,15 +1,64 @@
+import { useRef, useState } from "react";
 import { CDN_URL } from "../utils/constants";
 import { CuisinesSectionProps } from "../utils/types/props";
 
 const CuisinesSuggestions = ({ info }: CuisinesSectionProps) => {
+  const [translateValue, setTranslateValue] = useState(0);
+  const [maxTranslation, setMaxTranslation] = useState(false);
+  const cuisinesScrollRef = useRef<HTMLDivElement>(null);
+  const leftScroll = () => {
+    if (cuisinesScrollRef.current) {
+      cuisinesScrollRef.current.scrollBy({
+        left: -400,
+        behavior: "smooth",
+      });
+    }
+  };
+  const rightScroll = () => {
+    if (cuisinesScrollRef.current) {
+      cuisinesScrollRef.current.scrollBy({
+        left: 400,
+        behavior: "smooth",
+      });
+    }
+  };
+  const handleScroll = () => {
+    if (cuisinesScrollRef.current) {
+      setTranslateValue(cuisinesScrollRef.current.scrollLeft);
+      if (
+        cuisinesScrollRef.current?.scrollLeft >=
+        cuisinesScrollRef.current?.scrollWidth -
+          cuisinesScrollRef.current?.clientWidth
+      ) {
+        setMaxTranslation(true);
+      } else {
+        setMaxTranslation(false);
+      }
+    }
+  };
   const { header, imageGridCards } = info;
   const arr = imageGridCards?.info;
   return (
-    <div className="space-y-14 mx-auto ml-0 my-3 w-[85%] h-[230px] overflow-x-hidden overflow-y-hidden border-b-2 border-stone-300">
+    <div className="space-y-7 mx-auto ml-0 my-3 w-[85%] border-b-2 border-stone-300">
       <div className="flex justify-between">
         <h1 className="font-semibold text-xl">{header?.title}</h1>
         <div className="flex space-x-3">
-          <button className="font-bold text-xl text-stone-500 rounded-full p-2 bg-stone-300 bg-opacity-70 disabled:opacity-50">
+          <button
+            disabled={translateValue === 0}
+            className="font-bold text-xl text-stone-500 rounded-full p-2 bg-stone-300 bg-opacity-70 disabled:opacity-50"
+            onClick={leftScroll}
+          >
+            <img
+              src={"/arrow-prev-small-svgrepo-com.svg"}
+              alt="right arrow"
+              className="w-5 h-5"
+            />
+          </button>
+          <button
+            disabled={maxTranslation}
+            className="font-bold text-xl text-stone-500 rounded-full p-2 bg-stone-300 bg-opacity-70 disabled:opacity-50"
+            onClick={rightScroll}
+          >
             <img
               src={"/arrow-next-small-svgrepo-com.svg"}
               alt="left arrow"
@@ -17,22 +66,19 @@ const CuisinesSuggestions = ({ info }: CuisinesSectionProps) => {
               referrerPolicy="no-referrer"
             />
           </button>
-          <button className="font-bold text-xl text-stone-500 rounded-full p-2 bg-stone-300 bg-opacity-70 disabled:opacity-50">
-            <img
-              src={"/arrow-prev-small-svgrepo-com.svg"}
-              alt="right arrow"
-              className="w-5 h-5"
-            />
-          </button>
         </div>
       </div>
-      <div className={`flex space-x-24 mix-blend-multiply duration-300`}>
+      <div
+        className={`flex space-x-24 mix-blend-multiply duration-300 overflow-x-hidden overflow-y-hidden`}
+        ref={cuisinesScrollRef}
+        onScroll={handleScroll}
+      >
         {arr?.map((e) => (
-          <div key={e?.id} className="w-[7%]">
+          <div key={e?.id}>
             <img
               alt="banner"
               src={CDN_URL + e?.imageId}
-              className="relative scale-[200%] mx-9 object-cover"
+              className="relative scale-[200%] mx-9 object-cover my-12"
             />
           </div>
         ))}
