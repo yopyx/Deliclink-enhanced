c6568c4762bccd7216c8dc76c8dd89894a3ba502
Yomna Ali <yomnaaly83@gmail.com>
2024-09-27 20:21:21 +0300

    feat(MainPageError) : create MainPageError component UI structure which would be rendered upon error event in MainPage component

---
+++ src/components/error/MainPageError.tsx
@@ -0,0 +1,14 @@
+const MainPageError = () => {
+  return (
+    <div className="mt-10">
+      <img
+        src={"/fast-delivery-man-with-motorcycles-customers-order.jpg"}
+        alt="profile"
+        className="object-cover w-[60%] m-auto"
+        referrerPolicy="no-referrer"
+      />
+    </div>
+  );
+};
+
+export default MainPageError;
