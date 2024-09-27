7e74f6d8e295960575d8546f35b3885311812755
Yomna Ali <yomnaaly83@gmail.com>
2024-09-27 20:23:10 +0300

    feat(CollectionError) : create CollectionError component UI structure which would be rendered upon error event in Collection component (/collections path)

---
+++ src/components/error/CollectionError.tsx
@@ -0,0 +1,14 @@
+const CollectionError = () => {
+  return (
+    <div className="mt-10">
+      <img
+        src={"/organic-bag-icon-flat-vector-food-paper.jpg"}
+        alt="profile"
+        className="object-cover w-1/3 m-auto"
+        referrerPolicy="no-referrer"
+      />
+    </div>
+  );
+};
+
+export default CollectionError;
