366c31db1a0e71891724b85e21f3d227dc6a589c
Yomna Ali <yomnaaly83@gmail.com>
2024-09-29 23:09:58 +0300

    feat(connect to the store) : connect collection reducer to the store via adding the connection slice reducer to the store reducer

--- src/utils/redux/store.ts
+++ src/utils/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import geoLocationReducer from "./geoLocationsSlice";
 import filterReducer from "./filterSlice";
+import collectionReducer from "./collectionSlice";
 
 export const appStore = configureStore({
   reducer: {
     geoLocation: geoLocationReducer,
     filter: filterReducer,
+    collection: collectionReducer,
   },
 });
 export type RootState = ReturnType<typeof appStore.getState>;
