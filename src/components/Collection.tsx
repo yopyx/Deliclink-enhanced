15ad2263a6e527b9949b40efb2d1bdb792baaff0
Yomna Ali <yomnaaly83@gmail.com>
2024-09-29 22:50:41 +0300

    feat(passing props to Collection children components) : pass info, sortConfig and facet as props to FilterBar component and pass dataList as prop to CollectionCardsContainer component

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -2,9 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import getCollectionData from "../utils/functions/getCollectionData";
 import { useAppSelector } from "../utils/types/reactReduxHooks";
 import { FilterState, GeoLocationStateProp } from "../utils/types/slicesState";
-import { isCollectionInfo, isSortCard } from "../utils/constants";
+import {
+  isCollectionInfo,
+  isCollectionResData,
+  isGridCard2,
+  isSortCard,
+} from "../utils/constants";
 import FilterBar from "./FilterBar";
 import CollectionCardsContainer from "./CollectionCardsContainer";
+import { ResData, ResData2 } from "../utils/types/fetchedData";
 import CollectionShimmer from "./shimmer/CollectionShimmer";
 
 const Collection = () => {
@@ -42,7 +48,7 @@ const Collection = () => {
     return <div>{JSON.stringify(error)}</div>;
   }
   return (
-    <div className="w-[85%] my-14 mx-auto mr-0 flex flex-col space-y-10">
+    <div className="w-[85%] my-14 mx-auto mr-0 flex flex-col space-y-10 lg:page-lg md:page-md">
       <div className="flex flex-col gap-7">
         <h2 className="text-4xl font-semibold">{title}</h2>
         <p className="text-lg font-semibold text-stone-400">
@@ -52,8 +58,29 @@ const Collection = () => {
           }
         </p>
       </div>
-      {data!.data.cards.find((e) => isSortCard(e)) && <FilterBar />}
-      {<CollectionCardsContainer />}
+      {data!.data.cards.find((e) => isSortCard(e)) && (
+        <FilterBar
+          info={data!.data.cards.find((e) => isSortCard(e))!.card.card}
+          sortConfig={sortConfig}
+          facet={facets}
+        />
+      )}
+      {
+        <CollectionCardsContainer
+          dataList={[
+            ...data!.data.cards
+              .filter((e) => isCollectionResData(e))
+              .map((e) => e.card.card),
+            ...data!.data.cards
+              .filter((e) => isGridCard2(e))
+              .reduce(
+                (a, c) =>
+                  a.concat(c.card.card.gridElements.infoWithStyle.restaurants),
+                [] as (ResData | ResData2)[]
+              ),
+          ]}
+        />
+      }
     </div>
   );
 };
