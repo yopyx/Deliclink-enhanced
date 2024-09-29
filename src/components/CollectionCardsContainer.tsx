1f8101dfa5bd09db8fd11e367b05f28fcc04b9a0
Yomna Ali <yomnaaly83@gmail.com>
2024-09-29 22:54:14 +0300

    feat(CollectionCardsContainer data layer) : receive dataList as prop and render the list along Ui layer

--- src/components/CollectionCardsContainer.tsx
+++ src/components/CollectionCardsContainer.tsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
+import { CollectionCardsContainerProps } from "../utils/types/props";
 import RestaurantCard from "./RestaurantCard";
 
-const CollectionCardsContainer = () => {
+const CollectionCardsContainer = ({
+  dataList,
+}: CollectionCardsContainerProps) => {
   return (
-    <div className="flex flex-wrap">
-      {[].map((c, i) => (
-        <Link key={c!.info.id + i} to={"/restaurants/" + c!.info.id}>
-          <RestaurantCard resData={c!}></RestaurantCard>
-        </Link>
-      ))}
+    <div className="w-[85%] mx-auto ml-0 flex flex-col gap-y-7 overflow-y-hidden justify-center">
+      <div className="flex flex-wrap gap-x-1 mx-auto -ml-1 4k:res-container-4k 2xl:res-container-2xl lg:res-container-lg">
+        {(dataList || []).map((c, i) => (
+          <Link key={c!.info.id + i} to={"/restaurants/" + c!.info.id}>
+            <RestaurantCard resData={c!}></RestaurantCard>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
