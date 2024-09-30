e6ff381e55eb0617c0ce3a67b60f3a0b0190fe1a
Yomna Ali <yomnaaly83@gmail.com>
2024-09-30 22:22:01 +0300

    feat(adding states types) : create SearchState type for the state of stored search info data and create FacetInDetail type and add it to FilterState type props

--- src/utils/types/slicesState.ts
+++ src/utils/types/slicesState.ts
@@ -1,3 +1,5 @@
+import { SuggestionCard } from "./fetchedData";
+
 export type GeoLocationStateProp = {
   city: string;
   geometry: {
@@ -10,10 +12,23 @@ export type Sort = { sortTitle: string; sortKey: string };
 export type Facet = {
   [id: string]: { value: string }[];
 };
-
+export type FacetInDetail = {
+  [id: string]: { id: string; operator: string; label: string }[];
+};
 export type FilterState = {
   sortConfig: Sort;
   facets: Facet;
+  facetsInDetail: FacetInDetail;
 };
 
 export type CollectionState = { pathParams: string; title: string };
+
+export type SearchState = {
+  searchQuery: string;
+  history: { [id: string]: SuggestionCard[] };
+  selectedSuggestion: {
+    query: string;
+    meta: string;
+    type: string;
+  };
+};
