9dc898bbd25573db8e5d1bcd96136cba1afa8ffa
Yomna Ali <yomnaaly83@gmail.com>
2024-09-30 22:35:35 +0300

    feat(redux persist) : configure store persistence for each session by combining all slices reducers then we replace the default store reducer with persist reducer then export the persistor (persistent app store)

--- src/utils/redux/store.ts
+++ src/utils/redux/store.ts
@@ -1,14 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import geoLocationReducer from "./geoLocationsSlice";
 import filterReducer from "./filterSlice";
 import collectionReducer from "./collectionSlice";
+import storageSession from "redux-persist/lib/storage/session";
+import { persistStore, persistReducer } from "redux-persist";
 
-export const appStore = configureStore({
-  reducer: {
-    geoLocation: geoLocationReducer,
-    filter: filterReducer,
-    collection: collectionReducer,
-  },
+const rootReducer = combineReducers({
+  geoLocation: geoLocationReducer,
+  filter: filterReducer,
+  collection: collectionReducer,
 });
+const persistConfig = {
+  key: "root",
+  storage: storageSession,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const appStore = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+const persistor = persistStore(appStore);
 export type RootState = ReturnType<typeof appStore.getState>;
 export type AppDispatch = typeof appStore.dispatch;
+
+export { appStore, persistor };
