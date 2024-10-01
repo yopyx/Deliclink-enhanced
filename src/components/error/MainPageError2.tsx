de8ac7d832902bd2c448309160d2a1fbe930408c
Yomna Ali <yomnaaly83@gmail.com>
2024-10-01 21:27:24 +0300

    feat(MainPageError2) : create MainPageError2 component in case of unservicable location

---
+++ src/components/error/MainPageError2.tsx
@@ -0,0 +1,14 @@
+const MainPageError2 = () => {
+  return (
+    <div className="mt-10">
+      <img
+        src={"/fast-delivery-man-with-motorcycles-customers-order22.jpg"}
+        alt="profile"
+        className="object-cover w-[60%] m-auto"
+        referrerPolicy="no-referrer"
+      />
+    </div>
+  );
+};
+
+export default MainPageError2;
