26e037475df3a97fbccd6cc0ef5eafc552222edc
Yomna Ali <yomnaaly83@gmail.com>
2024-10-01 22:02:56 +0300

    feat(Search component) : create Ui layer structure of Search component which consists mainly of SearchSuggestions, SearchResults and CuisinesSuggestions components and query pre search cuisines using useQuery hook via executing getPreSearchCuisines to fetch the data needed and render them in CuisinesSuggestions component and allow debouncing through setting timeout to change search input every 500 ms after an input change happens using useRef hook

---
+++ src/components/Search.tsx
@@ -0,0 +1,63 @@
+import { useQuery } from "@tanstack/react-query";
+import { useAppDispatch, useAppSelector } from "../utils/types/reactReduxHooks";
+import { GeoLocationStateProp } from "../utils/types/slicesState";
+import getPreSearchCuisines from "../utils/functions/getPreSearchCuisines";
+import CuisinesSuggestions from "./CuisinesSuggestions";
+import { isCuisinesCard } from "../utils/constants";
+import { useRef, useState } from "react";
+import SearchSuggestions from "./SearchSuggestions";
+import { addSearchQuery } from "../utils/redux/searchSlice";
+import SearchResults from "./SearchResults";
+import SearchShimmer from "./shimmer/SearchShimmer";
+
+const Search = () => {
+  const dispatch = useAppDispatch();
+  const [viewSuggestions, setViewSuggestions] = useState(false);
+  const { geometry } = useAppSelector(
+    (store) => store.geoLocation.currentLocation
+  ) as GeoLocationStateProp;
+  const { searchQuery, selectedSuggestion } = useAppSelector(
+    (store) => store.search
+  );
+  const [text, setText] = useState("");
+  const debounceTimeout = useRef<NodeJS.Timeout | null>(null);
+  const { data, status } = useQuery({
+    queryKey: ["Search", geometry.lat, geometry.lng],
+    queryFn: () => getPreSearchCuisines(geometry.lat, geometry.lng),
+  });
+  const handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+    if (debounceTimeout.current) clearTimeout(debounceTimeout.current);
+    debounceTimeout.current = setTimeout(() => {
+      dispatch(addSearchQuery(e.target.value));
+    }, 500);
+  };
+  if (status === "pending") {
+    return <SearchShimmer />;
+  }
+  return (
+    <div className="w-[65%] my-32 mx-auto flex flex-col gap-y-36 overflow-x-hidden">
+      <div className="flex flex-col gap-y-5">
+        <input
+          type="text"
+          value={text}
+          onChange={handleQueryChange}
+          onFocus={() => setViewSuggestions(true)}
+          onBlur={() => setTimeout(() => setViewSuggestions(false), 200)}
+          placeholder="Search for restaurants and food"
+          className="p-2 relative z-[15]"
+        />
+        {viewSuggestions && searchQuery && <SearchSuggestions />}
+        {selectedSuggestion?.query && <SearchResults />}
+      </div>
+      {data!.data.cards.find((e) => isCuisinesCard(e)) && (
+        <CuisinesSuggestions
+          info={data!.data.cards.find((e) => isCuisinesCard(e))!.card.card}
+          updateText={setText}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Search;
