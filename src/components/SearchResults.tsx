a017093bd24f42f07db803474562b9ef74c9f2d1
Yomna Ali <yomnaaly83@gmail.com>
2024-10-02 21:37:33 +0300

    feat(SearchResults component) : create Ui layer structure of SearchResults component which consists mainly of FilterBar, SearchResultedResCard and SearchResultedDishCard components and query search results using useQueries hook via executing getSearchResults in two queries objects which fetches the data needed categorized into types(DISH/RESTAURANT) and render sort card data in FilterBar component

---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,107 @@
+import { useQueries } from "@tanstack/react-query";
+import getSearchResults from "../utils/functions/getSearchResults";
+import { SearchResultsProps } from "../utils/types/props";
+import { FilterState } from "../utils/types/slicesState";
+import { useAppSelector } from "../utils/types/reactReduxHooks";
+import { isDishResCard, isSortCard } from "../utils/constants";
+import FilterBar from "./FilterBar";
+import { useState } from "react";
+import SearchResultedResCard from "./SearchResultedResCard";
+import { Link } from "react-router-dom";
+import SearchResultedDishCard from "./SearchResultedDishCard";
+
+const SearchResults = ({ lat, lng, query, meta, type }: SearchResultsProps) => {
+  const { sortConfig, facets, facetsInDetail }: FilterState = useAppSelector(
+    (store) => store.filter
+  );
+  const [resCategory, setResCategory] = useState(type === "RESTAURANT");
+  const data = useQueries({
+    queries: [
+      {
+        queryKey: [
+          "search resulted dishes",
+          lat,
+          query,
+          meta,
+          sortConfig.sortKey,
+          JSON.stringify(facets),
+        ],
+        queryFn: () =>
+          getSearchResults(
+            lat,
+            lng,
+            query,
+            meta,
+            "DISH",
+            sortConfig.sortKey,
+            facetsInDetail
+          ),
+      },
+      {
+        queryKey: ["search resulted restaurants", lat, query, meta],
+        queryFn: () =>
+          getSearchResults(
+            lat,
+            lng,
+            query,
+            meta,
+            "RESTAURANT",
+            sortConfig.sortKey,
+            facetsInDetail
+          ),
+      },
+    ],
+  });
+  if (data[0].status === "pending" || data[1].status === "pending") {
+    return <div>loading...</div>;
+  }
+  return (
+    <div className="w-[65%] mx-auto ml-0 flex flex-col gap-y-5 absolute my-14 rounded-lg">
+      <div className="space-x-3">
+        {["Restaurants", "Dishes"].map((e) => (
+          <button
+            key={e}
+            className={`${
+              (e === "Restaurants" && resCategory) ||
+              (e === "Dishes" && !resCategory)
+                ? "bg-slate-300 text-black"
+                : "bg-orange-600 text-white"
+            } text-sm rounded-full p-2 hover:bg-slate-300 hover:text-black`}
+            onClick={() => setResCategory(e[0] === "R")}
+          >
+            {e}
+          </button>
+        ))}
+      </div>
+      {!resCategory &&
+        data[0].data?.data.cards[0]?.groupedCard?.cardGroupMap?.DISH?.cards?.find(
+          (e) => isSortCard(e)
+        ) && (
+          <FilterBar
+            info={
+              data[0].data?.data.cards[0].groupedCard.cardGroupMap.DISH.cards.find(
+                (e) => isSortCard(e)
+              )!.card.card
+            }
+            sortConfig={sortConfig}
+            facet={facets}
+          />
+        )}
+      <div className="bg-sunset shadow-inner shadow-yellow-600 p-5 z-[5] mx-auto ml-0 flex flex-wrap justify-between gap-y-5 rounded-lg">
+        {resCategory
+          ? [].map((e, i) => (
+              <Link key={e.info.id + i} to={"/restaurants/" + e.info.id}>
+                <SearchResultedResCard />
+              </Link>
+            ))
+          : []
+              .filter((e) => isDishResCard(e))
+              .map((e, i) => (
+                <SearchResultedDishCard key={e.card.card.info.id + i} />
+              ))}
+      </div>
+    </div>
+  );
+};
+
+export default SearchResults;
