b15ae09535cc339a6832de74ef914748a508c0ee
Yomna Ali <yomnaaly83@gmail.com>
2024-10-04 23:53:11 +0300

    feat(sized screens in "tailwind.config") : create 7 screen sizes for default spectrum of devices screens (xs/sm/md/lg/2xl/4k)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,scss}"],
   theme: {
     extend: {},
+    screens: {
+      xs: { max: "320px" },
+      sm: { max: "375px" },
+      md: { max: "786px" },
+      lg: { max: "1206px" },
+      "2xl": { max: "1510px", min: "1207px" },
+      "4k": { max: "2638px", min: "1509px" },
   },
   plugins: [],
 };
