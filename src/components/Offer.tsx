90277d54d74ed61b0d44bb2f8be46a849dd6e46c
Yomna Ali <yomnaaly83@gmail.com>
2024-10-05 23:57:02 +0300

    refactor : refactor styles in Offer component and remove ReactQueryDevtools component from main file

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -3,7 +3,7 @@ import { OfferProps } from "../utils/types/props";
 const Offer = ({ info }: OfferProps) => {
   const { header, couponCode, description } = info;
   return (
-    <div className="bg-[#eeeeee80] w-[185px] h-[100px] p-2 mx-2 text-center border-4 border-dashed border-stone-400">
+    <div className="bg-[#eeeeee80] w-[185px] h-[120px] p-2 mx-2 text-center border-4 border-dashed border-stone-400">
       <h4>{header}</h4>
       <h6>{couponCode}</h6>
       <h6>{description ? "| " + description + " |" : ""}</h6>
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./final.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools />
     </QueryClientProvider>
   </StrictMode>
 );
