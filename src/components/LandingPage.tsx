c269b43e588966481cdcd0be72861cd0abf7277e
Yomna Ali <yomnaaly83@gmail.com>
2024-10-05 23:42:35 +0300

    feat(LandingPage Ui styling) : refactor and edit LandingPage Ui layer styles to fit within 1100px

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -20,30 +20,29 @@ const LandingPage = () => {
   //   return data?.filter((e) => e.components.country === "India");
   // }, [data]);
   return (
-    <div className="flex flex-col">
-      <div className="flex justify-between mx-36 rounded-2xl shadow-lg bg-stone-400 bg-opacity-30">
-        <div className="w-3/5 p-14 flex flex-col justify-evenly space-y-8">
+    <div className="w-[1100px] flex flex-col mx-auto text-nowrap">
+      <div className="flex mx-auto justify-between rounded-2xl shadow-lg bg-stone-400 bg-opacity-30">
+        <div className="p-14 flex flex-1 flex-col space-y-14">
           <div className="flex gap-x-2">
             <h2 className="font-semibold font-sans text-[30px]">
               Only a few clicks to place{" "}
-              <span className="text-orange-600 font-bold">your order .</span>
+              <span className="text-orange-600 font-bold">your order . .</span>
             </h2>
-            <div className="flex h-max -mx-5">
+            <div className="flex h-10 -mx-5">
               <img
                 src={"/location-indicator.svg"}
-                className="h-12 -mt-3"
+                className="-mt-3 object-cover"
                 alt="location symbol"
                 referrerPolicy="no-referrer"
               />
             </div>
-            <span className="text-orange-600 font-bold text-[30px]">. .</span>
           </div>
-          <div className="space-y-5">
+          <div className="flex flex-col gap-y-5">
             <h3 className="text-lg text-slate-500 font-semibold">
               Order your favourite food from restaurants near you
             </h3>
             <div className="flex">
-              <div className="flex flex-col w-[61%]">
+              <div className="flex flex-col w-96">
                 <input
                   type="text"
                   value={searchText}
@@ -61,7 +60,7 @@ const LandingPage = () => {
                   </p>
                 ) : (
                   data && (
-                    <ul className="w-1/4 bg-white absolute mt-11 p-1 rounded-b-lg">
+                    <ul className="w-96 bg-white absolute mt-11 p-1 rounded-b-lg z-10">
                       {data!.map((e, i) => (
                         <li
                           key={e.formatted + i}
@@ -112,7 +111,7 @@ const LandingPage = () => {
           </div>
           <div className="flex flex-col gap-y-3">
             <h3 className="text-lg">Cities within our reach in India</h3>
-            <div className="flex flex-wrap gap-x-3 w-3/5 font-semibold text-stone-500">
+            <div className="flex flex-wrap gap-x-3 w-80 font-semibold text-stone-500">
               {[
                 {
                   city: "ahmedabad",
@@ -162,7 +161,7 @@ const LandingPage = () => {
             </div>
           </div>
         </div>
-        <div className="w-1/3 flex overflow-x-hidden shadow-lg lg:hidden">
+        <div className="flex flex-1 shadow-lg lg:hidden">
           <img src={main} alt="background" className="object-cover" />
         </div>
       </div>
