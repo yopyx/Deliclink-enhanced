f3c92bf52b9c12f5f5a5f75c71e4f9ecd4291669
Yomna Ali <yomnaaly83@gmail.com>
2024-10-06 09:36:49 +0300

    feat(Header subscribing to store and apply styling classes for various screens) : subscribe to geoLocation slice via useAppSelector to allow linking to MainPage component containing current restaurants data through Home nav element and subscribe to cart slice via useAppSelector to allow showing counts of items added at cart nav element. add "search" nav element which links to /search path and remove "About Us" and "Contact Us" elements. apply header-lg class at lg-h screen size and header-md class at md-h screen size for main div and apply logo-lg class at lg-h screen size and logo-md class at md-h screen size for logo img element and apply header-list-md class at md-h screen size for nav list

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,69 +1,81 @@
 import logo from "/deliclink-logo11.png";
 import cart from "/cart.png";
 import { Link, useLocation } from "react-router-dom";
-import { PROFILE_ICON } from "../utils/constants";
+import { useAppSelector } from "../utils/types/reactReduxHooks";
+import { GeoLocationStateProp } from "../utils/types/slicesState";
 
 const Header = () => {
   const { pathname } = useLocation();
+  const { city } = useAppSelector(
+    (store) => store.geoLocation.currentLocation
+  ) as GeoLocationStateProp;
+  const { items } = useAppSelector((store) => store.cart);
   return pathname === "/" ? (
     <div className="flex justify-between my-6 mx-40">
       <div className="w-48 cursor-pointer -mt-9">
         <img src={logo} alt="logo" className="object-cover" />
       </div>
-      <Link to="/signin">
-        <div className="flex space-x-1 px-2 py-1 mt-3 text-white bg-orange-600 rounded-lg border-white border-2 duration-200 hover:bg-stone-300 hover:text-black">
+      <Link to="">
+        <div className="flex space-x-1 px-2 py-1 mt-3 lg:mt-0 text-white bg-orange-600 rounded-lg border-white border-2 duration-200 hover:bg-stone-300 hover:text-black">
           <img
-            src={PROFILE_ICON}
+            src={"/user-avatar-filled.svg"}
             alt="profile"
-            className="w-8 h-8 mix-blend-plus-lighter"
+            className="w-8 h-8 opacity-50"
+            referrerPolicy="no-referrer"
           />
           <button className="text-sm">Signup / Login</button>
         </div>
       </Link>
     </div>
   ) : (
-    <div className="flex justify-between sticky top-0 bg-sunset h-20 shadow-3xl font-medium z-first">
-      <div className="w-56">
+    <div
+      className={`w-[100%] mx-auto flex justify-between px-14 h-20 sticky top-0 bg-sunset shadow-3xl font-medium z-20 ${
+        ["/restaurants", "/cart", "/search"].includes(pathname)
+          ? "lg-h:w-[950px] md-h:w-[950px]"
+          : "lg-h:header-lg md-h:header-md"
+      }`}
+    >
+      <div className="">
         <Link to="/">
           <img
             src={logo}
             alt="logo"
-            className="object-cover cursor-pointer -mt-10 ml-20"
+            className="object-cover cursor-pointer absolute h-32 -mt-7 lg-h:logo-lg md-h:logo-md"
           />
         </Link>
       </div>
       <div className="flex items-center">
-        <ul className="flex pr-12 cursor-pointer">
-          <Link to={"/"}>
+        <ul className="flex cursor-pointer md-h:header-list-md">
+          <Link to={`/city/${city}`}>
             <li className="px-6 py-7 bg-main-orange-hover hover:text-white">
               Home
             </li>
           </Link>
-          <Link to="/about">
-            <li className="px-6 py-7 bg-main-orange-hover hover:text-white">
-              About Us
-            </li>
-          </Link>
-          <Link to="/contact">
-            <li className="px-6 py-7 bg-main-orange-hover hover:text-white">
+          <Link to="">
+            <li className="text-nowrap px-6 py-7 bg-main-orange-hover hover:text-white">
               Contact Us
             </li>
           </Link>
           <Link to="/cart">
             <li className="px-6 py-7 bg-main-orange-hover text-st_orange hover:text-white">
               <div className="flex w-full h-6">
+                <span>
+                  {Object.values(items).reduce((a, c) => a + c.num, 0) || ""}
+                </span>
                 <img
                   id="cart"
                   src={cart}
                   className="ml-1 -mb-6 h-6"
                   alt="cart"
+                  referrerPolicy="no-referrer"
                 />
               </div>
             </li>
           </Link>
-          <Link to="/signin">
-            <li className="px-6 py-7 bg-main-orange-hover hover:text-white">
-              Signup/Login
+          <Link to="/search">
+            <li className="flex gap-x-3 px-6 py-7 bg-main-orange-hover hover:text-white">
+              <span className="text-2xl -my-1 -rotate-90">⌕</span>{" "}
+              <span className="lg:hidden">Search</span>
             </li>
           </Link>
         </ul>
