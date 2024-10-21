1e92eb4b59927b0a09a8f44671047a1495ec7d02
Yomna Ali <yomnaaly83@gmail.com>
2024-10-21 23:07:53 +0300

    feat(implement resetting scroll value) : add ScrollToTop component at the top of Body component since ScrollToTop reset scroll value to (0,0) whenever location pathname is changed

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import ScrollToTop from "./ScrollToTop";
 
 const Body = () => {
   return (
     <div className="flex flex-col">
+      <ScrollToTop />
       <Header />
       <Outlet />
     </div>
