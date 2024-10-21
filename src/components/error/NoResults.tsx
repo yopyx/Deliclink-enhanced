65ce7074fe3aa6447520bb63a3e96ac446bc4561
Yomna Ali <yomnaaly83@gmail.com>
2024-10-21 23:09:11 +0300

    feat(NoResults) : create NoResults component that would be rendered in case of empty data structure in various components

---
+++ src/components/error/NoResults.tsx
@@ -0,0 +1,9 @@
+const NoResults = () => {
+  return (
+    <div className="w-[100%] py-2 mx-auto ml-0 text-center font-semibold text-lg text-zinc-500 border-2 border-orange-500 bg-orange-100 rounded-lg">
+      -No results-
+    </div>
+  );
+};
+
+export default NoResults;
