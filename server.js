24827eec6c57725b30e654aac6807ee08472974f
Yomna Ali <yomnaaly83@gmail.com>
2024-11-16 23:58:15 +0200

    feat(express proxy server) : initialize express server and use all routes created by connecting them to app server and listen to port 3001

---
+++ server.js
@@ -0,0 +1,31 @@
+//development
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import collectionRouter from "./routes/collection.js";
+import resListRouter from "./routes/res-list.js";
+import cityDataRouter from "./routes/city-res-data.js";
+import geoRouter from "./routes/geo-suggest.js";
+import menuDataRouter from "./routes/menu-data.js";
+import preCuisinesRouter from "./routes/pre-search-cuisines.js";
+import searchResultsRouter from "./routes/search-results.js";
+import searchSuggestRouter from "./routes/search-suggest.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const environment = "development";
+const PORT = 3001;
+
+app.use("/", collectionRouter);
+app.use("/", geoRouter);
+app.use("/", resListRouter);
+app.use("/", cityDataRouter);
+app.use("/", geoRouter);
+app.use("/", menuDataRouter);
+app.use("/", preCuisinesRouter);
+app.use("/", searchResultsRouter);
+app.use("/", searchSuggestRouter);
+
+app.listen(PORT);
