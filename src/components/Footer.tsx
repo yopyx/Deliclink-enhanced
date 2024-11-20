7b13b93ac6f6f1e6af84033524dad717607280d4
Yomna Ali <yomnaaly83@gmail.com>
2024-11-20 23:58:47 +0200

    feat(styling Footer) : add styles in case of md-h and mobile screens and hide the second div in md screen

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
 const Footer = () => {
   return (
-    <div className="flex flex-col">
+    <div className="w-full flex flex-col">
       <div className="bg-orange-950 h-48 flex items-center">
-        <h2 className="w-max mx-auto text-white text-3xl font-bold">
+        <h2 className="w-max mx-auto text-white text-3xl font-bold md-h:text-2xl mobile:text-lg">
           %Offers% everyday and more..
         </h2>
       </div>
-      <div className="mx-auto flex justify-evenly">
+      <div className="mx-auto flex justify-evenly md:hidden">
         <img
           alt="mobile app1"
           src="/footer1.png"
