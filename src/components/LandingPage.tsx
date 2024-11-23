0634ff6b639ba58f24abd4d2f54d8549f7671baa
Yomna Ali <yomnaaly83@gmail.com>
2024-11-23 23:54:40 +0200

    feat(styling LandingPage) : add styling classes for elements at md-h and mobile screens and refactor some classes for responsive screens at various size limitations

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -8,15 +8,15 @@ import GeoSearch from "./GeoSearch";
 const LandingPage = () => {
   const dispatch = useAppDispatch();
   return (
-    <div className="w-[1100px] flex flex-col mx-auto text-nowrap">
-      <div className="flex mx-auto justify-between rounded-2xl shadow-lg bg-stone-400 bg-opacity-30">
-        <div className="p-14 flex flex-1 flex-col space-y-14">
+    <div className="w-full flex flex-col mx-auto ml-0">
+      <div className="w-2/3 lp-sub2:w-full grid grid-flow-col mx-auto justify-between rounded-t-2xl shadow-lg bg-stone-400 bg-opacity-30">
+        <div className="p-14 mobile:py-5 mobile:px-2 grid-cols-7 my-auto flex flex-1 flex-col space-y-14">
           <div className="flex gap-x-2">
-            <h2 className="font-semibold font-sans text-[30px]">
+            <h2 className="font-semibold font-sans text-[30px] md-h:text-2xl mobile:text-sm">
               Only a few clicks to place{" "}
               <span className="text-orange-600 font-bold">your order . .</span>
             </h2>
-            <div className="flex h-10 -mx-5">
+            <div className="flex h-10 -mx-5 mobile:h-3 mobile:-mx-3">
               <img
                 src={"/location-indicator.svg"}
                 className="-mt-3 object-cover"
@@ -26,14 +26,16 @@ const LandingPage = () => {
             </div>
           </div>
           <div className="flex flex-col gap-y-5">
-            <h3 className="text-lg text-slate-500 font-semibold">
+            <h3 className="text-lg text-slate-500 font-semibold mobile:text-xs">
               Order your favourite food from restaurants near you
             </h3>
             <GeoSearch />
           </div>
           <div className="flex flex-col gap-y-3">
-            <h3 className="text-lg">Cities within our reach in India</h3>
-            <div className="flex flex-wrap gap-x-3 w-80 font-semibold text-stone-500">
+            <h3 className="text-lg mobile:text-sm">
+              Cities within our reach in India
+            </h3>
+            <div className="flex flex-wrap gap-x-3 w-80 font-semibold text-stone-500 mobile:text-xs">
               {[
                 {
                   city: "ahmedabad",
@@ -83,8 +85,8 @@ const LandingPage = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-1 shadow-lg lg:hidden">
-          <img src={main} alt="background" className="object-cover" />
+        <div className="grid-cols-5 flex shadow-lg lp-sub1:hidden">
+          <img src={main} alt="background" className="object-cover w-96" />
         </div>
       </div>
       <Footer />
