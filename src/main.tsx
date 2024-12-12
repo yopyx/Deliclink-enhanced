4ddda602ee31f7e567fb019ce63f1671cd892ce4
Yomna Ali <yomnaaly83@gmail.com>
2024-12-12 05:04:11 +0200

    build : allow vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./final.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { inject } from "@vercel/analytics";
+inject();
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
